17003f97c501627fec47f71f44e297f3928cb292
Preeti Reddy <60726612+Preeti330@users.noreply.github.com>
2024-02-09 05:48:26 +0000

    comments

--- basics/basics_01.js
+++ basics/basics_01.js
@@ -1,1 +1,4 @@
 console.log("hello");
+//configure the run enveronment node js in code space .
+// go to file -> view -> command pallate -> select codespace add div container config 
+// to save file -> go extentions symbol -> add message to commit  -> merage 
