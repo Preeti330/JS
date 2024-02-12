d44e79512fe4bf3d0d8ad09666e26c683f8355b3
Preeti Reddy <60726612+Preeti330@users.noreply.github.com>
2024-02-12 06:19:35 +0000

    basics _1 is completed (number,string,math,date)

---
+++ basics/basics_03_datatype.js
@@ -0,0 +1,12 @@
+console.log(2==1);
+console.log(2>=1);
+console.log(2<=1);
+
+console.log("2">1); // string and int 
+console.log("02">=1); //02 is converts 2 , 2>=1
+
+console.log(null==0); // equlity comparison , return false bcz value of null will null 
+console.log(null>=0); // comparision , here null value will be 0 , 
+
+console.log(undefined==0);
+console.log(undefined>=0);
---
+++ basics/basics_04_datatypeconversion.js
@@ -0,0 +1,21 @@
+console.log("3"+ 2 + 1);//321
+console.log(3 + "3" + 3);//333
+console.log(3+4+"5")//;75
+
+let n="2";
+console.log(typeof n); // string 
+let n1=Number(n);
+//"2" => 2
+// "3hiii"=>NaN
+//""=>0
+//ture => 1,false => 0
+//undefined => NaN
+
+let b=0;
+console.log(typeof b); //ture  
+let b2=Boolean(b)
+//0=>false,1=>ture
+//""=>false,
+//"jijij"=>ture
+//null=>false
+
